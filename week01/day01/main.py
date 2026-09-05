ad3be63c8cf89f6a7378ee3327793a103382ccf2
Артур Кроян <kroyan-a@mail.ru>
2026-09-05 20:11:21 +0300

    Настройка окружения и создание визитки

---
+++ week01/day01/main.py
@@ -0,0 +1,10 @@
+name = "Артур"
+group = "ИП-123"
+age = 23
+interests = "Прогр, тестирование ПО"
+print("===Визитка===")
+print(f"Имя:{name}")
+print(f"Группа:{group}")
+print(f"Возраст:{age}")
+print(f"Интересы:{interests}")
+
